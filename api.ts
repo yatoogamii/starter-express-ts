fc290cdaa8fc11427a7423e72c6ba91ec9360d1a
yatoogamii <bracaval.elias.pro@gmail.com>
2019-12-17 14:59:55 +0100

    first commit

---
+++ api.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import { Request, Response } from "express";
+const app = express();
+const port = 8080; // default port to listen
+
+// define a route handler for the default home page
+app.get( "/", (req: Request, res: Response ) => {
+    res.send( "Hello world!" );
+} );
+
+// start the Express server
+app.listen( port, () => {
+    console.log( `server started at http://localhost:${ port }` );
+} );
